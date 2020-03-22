080c26d574183fa803344d42d49691ae08863b33
Alicia Lancaster <alancaster123@yahoo.com>
2020-03-22 11:49:53 -0700

    Week 2-Task 3 Completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Directory from './components/DirectoryComponent';
 import './App.css';
-import {CAMPSITES} from './shared/campsite';
+import { CAMPSITES } from './shared/campsite';
+{
+  /*import CampsiteInfo from './components/CampsiteInfoComponent';*/
+}
 
 class App extends Component {
   constructor(props) {
@@ -12,18 +15,19 @@ class App extends Component {
     };
   }
 
-    render() {
-        return (
-            <div className="App">
-                <Navbar dark color="primary">
-                <div className="container">
-                    <NavbarBrand href="/">NuCamp</NavbarBrand>
-                </div>
-                </Navbar>
-                <Directory campsites={this.state.campsites} />
-            </div>
-        );
-    };
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">NuCamp</NavbarBrand>
+          </div>
+        </Navbar>
+        <Directory campsites={this.state.campsites} />
+        {/*<CampsiteInfo campsites={this.state.campsites} /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
