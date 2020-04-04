71a14ddf4b0f6fb1db1c855c63d2a6c91b18f448
Alicia Lancaster <alancaster123@yahoo.com>
2020-04-04 12:10:56 -0700

    Week 4 Assignment
    Created a Form Modal using React Redux

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   Card,
   CardImg,
@@ -6,9 +6,124 @@ import {
   CardBody,
   CardTitle,
   Breadcrumb,
-  BreadcrumbItem
+  Button,
+  BreadcrumbItem,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Label
 } from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+
+const maxLength = len => val => !val || val.length <= len;
+const minLength = len => val => val && val.length >= len;
+
+class CommentForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isModalOpen: false
+    };
+
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  }
+
+  handleSubmit(values) {
+    console.log(
+      //`Rating: ${this.rating.value} Author: ${this.author.value} Text: ${this.text.value}`
+      'Current state is ' + JSON.stringify(values)
+    );
+    alert(
+      //`Rating: ${this.rating.value} Author: ${this.author.value} Text: ${this.text.value}`
+      'Current state is ' + JSON.stringify(values)
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <Button outline onClick={this.toggleModal}>
+          <i className="fa fa-pencil fa-lg" />
+          Submit Comment
+        </Button>
+
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}> Submit Comment </ModalHeader>
+          <ModalBody>
+            <LocalForm
+              onSubmit={values => {
+                this.handleSubmit(values);
+              }}
+            >
+              <div className="form-group">
+                <Label htmlFor="rating">Rating</Label>
+                <Control.select
+                  className="form-control"
+                  model=".rating"
+                  id="rating"
+                  name="rating"
+                >
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </Control.select>
+              </div>
+              <div className="form-group">
+                <Label htmlFor="author">Your Name</Label>
+                <Control.text
+                  className="form-control"
+                  model=".author"
+                  id="author"
+                  name="author"
+                  placeholder="Your Name"
+                  validators={{
+                    minLength: minLength(2),
+                    maxLength: maxLength(15)
+                  }}
+                />
+                <Errors
+                  className="text-danger"
+                  model=".author"
+                  show="touched"
+                  component="div"
+                  messages={{
+                    minLength: 'Must be at least 2 characters',
+                    maxLength: 'Must be 15 characters or less'
+                  }}
+                />
+              </div>
+              <div className="form-group">
+                <Label htmlFor="text">Comments</Label>
+
+                <Control.textarea
+                  className="form-control"
+                  rows="6"
+                  model=".text"
+                  id="text"
+                  name="text"
+                />
+              </div>
+              <Button type="submit" color="primary">
+                Submit
+              </Button>
+            </LocalForm>
+          </ModalBody>
+        </Modal>
+      </div>
+    );
+  }
+}
 
 function RenderCampsite({ campsite }) {
   return (
@@ -47,6 +162,9 @@ function RenderComments({ comments }) {
             </div>
           );
         })}
+        <div>
+          <CommentForm />
+        </div>
       </div>
     );
   }
